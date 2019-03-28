7044897c1d6c0f232a76ce3dd8fb7538ebde380f
tranchung <tranducchung1st@gmail.com>
2019-03-28 17:19:51 +0700

    Done

--- src/main/java/CheckPrime/CheckPrime.java
+++ src/main/java/CheckPrime/CheckPrime.java
@@ -9,8 +9,12 @@ public class CheckPrime {
         System.out.println("Enter number you want check ");
         number = scanner.nextInt();
         boolean check = true;
+
         for(int i=2;i<=Math.sqrt(number);i++){
             if(number%i == 0){
+                check = false;
+            }
+            if(number<=2){
                 check = false;
             }
         }
